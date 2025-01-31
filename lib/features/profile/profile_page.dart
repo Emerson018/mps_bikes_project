8bb22284ca3355b27cf868bd4db4cb9b6ac7907f
Emerson Viçosa <emersonvicosa@gmail.com>
2025-01-30 21:51:52 -0300

    V32.1, signout feature

--- lib/features/profile/profile_page.dart
+++ lib/features/profile/profile_page.dart
@@ -1,10 +1,43 @@
-import 'package:flutter/widgets.dart';
+import 'package:flutter/material.dart';
+import 'package:mps_app/locator.dart';
+import 'package:mps_app/services/auth_service.dart';
+import 'package:mps_app/services/secure_storage.dart';
 
-class ProfilePage extends StatelessWidget {
+class ProfilePage extends StatefulWidget {
   const ProfilePage({super.key});
 
   @override
-  Widget build(BuildContext context) {
-    return const Placeholder();
-  }
+  State<ProfilePage> createState() => _ProfilePageState();
 }
+
+class _ProfilePageState extends State<ProfilePage> 
+  with AutomaticKeepAliveClientMixin<ProfilePage> {
+  
+  @override
+  bool get wantKeepAlive => true;
+
+  @override
+  Widget build(BuildContext context) {
+    super.build(context);
+    return Scaffold(
+      body: Center(
+        child: Column(
+          mainAxisAlignment:  MainAxisAlignment.center,
+          children: [
+            Text("Profile"),
+            TextButton(
+              onPressed: () async {
+                await locator.get<AuthService>().signOut();
+                await const Securestorage().deleteAll();
+                if (mounted) {
+                  Navigator.popUntil(context, ModalRoute.withName('/'));
+                }
+              },
+              child: Text("Logout")
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+  }
