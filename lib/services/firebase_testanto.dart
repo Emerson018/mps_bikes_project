9c819bca5a77326abc04a8c13d2e6cf998c21081
Emerson Viçosa <emersonvicosa@gmail.com>
2025-02-07 22:59:31 -0300

    app funcionando, falta conectar no hasura

---
+++ lib/services/firebase_testanto.dart
@@ -0,0 +1,84 @@
+import 'dart:developer';
+import 'package:cloud_functions/cloud_functions.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:mps_app/common/models/user_model.dart';
+import 'package:mps_app/services/auth_service.dart';
+
+class FirebaseAuthService implements AuthService {
+  final _auth = FirebaseAuth.instance;
+  final _functions = FirebaseFunctions.instance;
+
+  @override
+  Future<UserModel> signIn({
+    required String email,
+    required String password,
+  }) async {
+    try {
+     final result = await _auth.signInWithEmailAndPassword(
+      email: email,
+      password: password
+    );
+    if(result.user != null) {
+      return UserModel(
+        name: result.user!.displayName,
+        email: result.user!.email,
+        id: result.user!.uid,
+         );
+    } else {
+      throw Exception();
+    }
+   } on FirebaseAuthException catch (e) {
+    throw e.message ?? "null";
+   } catch (e) {
+    rethrow;
+   }
+  }
+
+  @override
+  Future<UserModel> signUp({
+    String? name,
+    required String email,
+    required String password
+  }) async{
+   try {
+    await _functions.httpsCallable('registerUser').call({
+            "email": email,
+            "password": password,
+            "displayName": name
+        });
+
+    final result = await _auth.signInWithEmailAndPassword(
+      email: email,
+      password: password
+    );
+
+    if(result.user != null) {
+      log(await _auth.currentUser?.getIdToken(true) ?? 'nulo');
+
+      await result.user!.updateDisplayName(name);
+      return UserModel(
+        name: _auth.currentUser?.displayName,
+        email: _auth.currentUser?.email,
+        id: _auth.currentUser?.uid,
+         );
+    } else {
+      throw Exception();
+    }
+   } on FirebaseAuthException catch (e) {
+    throw e.message ?? "null";
+   } on FirebaseFunctionsException catch (e) { 
+    throw e.message ?? "null";
+   } catch (e) {
+    rethrow;
+   }
+  }
+  
+  @override
+  Future<void> signOut() async {
+    try {
+      await _auth.signOut();
+    } catch (e) {
+      rethrow;
+    }
+  }
+}
